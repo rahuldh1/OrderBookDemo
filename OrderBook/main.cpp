ed547143c02daee11cfa8a249b07e33dcdfd78b6
rahuldh1 <rahuldh1@yahoo.com>
2016-09-30 16:57:30 +0530

    1 million orders tested
    1 million orders tested

--- OrderBook/main.cpp
+++ OrderBook/main.cpp
@@ -14,21 +14,16 @@ int _tmain(int argc, _TCHAR* argv[])
 	std::thread t(&OrderBook::run, orderBook);
 
 
-	//TEST ORDER
-	orderQueue.putOrderInQueue( Order("Trader1","Stock1",100,ORDER_SIDE::SELL) );
-	orderQueue.putOrderInQueue( Order("Trader2","Stock1",200,ORDER_SIDE::BUY) );
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	orderQueue.putOrderInQueue( Order("Trader3","Stock1",100,ORDER_SIDE::BUY) );
-	orderQueue.putOrderInQueue( Order("Trader5","Stock1",100,ORDER_SIDE::BUY) );
-	orderQueue.putOrderInQueue( Order("Trader6","Stock1",100,ORDER_SIDE::BUY) );
-	orderQueue.putOrderInQueue( Order("Trader7","Stock1",100,ORDER_SIDE::BUY) );
-	orderQueue.putOrderInQueue( Order("Trader8","Stock1",100,ORDER_SIDE::BUY) );
-	orderQueue.putOrderInQueue( Order("Trader9","Stock1",100,ORDER_SIDE::BUY) );
-	orderQueue.putOrderInQueue( Order("Trader10","Stock1",100,ORDER_SIDE::BUY) );
-	orderQueue.putOrderInQueue( Order("Trader11","Stock1",100,ORDER_SIDE::BUY) );
-	orderQueue.putOrderInQueue( Order("Trader12","Stock1",100,ORDER_SIDE::BUY) );
-	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-	orderQueue.putOrderInQueue( Order("Trader4","Stock1",300,ORDER_SIDE::SELL) );
+	//TEST ORDER (1 million Buy and Sell orders on multiple stocks are placed)
+	orderQueue.putOrderInQueue( Order("Trader1","Stock1",250000,ORDER_SIDE::SELL) );
+	orderQueue.putOrderInQueue( Order("Trader2","Stock2",250000,ORDER_SIDE::SELL) );
+	orderQueue.putOrderInQueue( Order("Trader3","Stock3",250000,ORDER_SIDE::SELL) );
+	orderQueue.putOrderInQueue( Order("Trader5","Stock1",250000,ORDER_SIDE::BUY) );
+	orderQueue.putOrderInQueue( Order("Trader6","Stock2",250000,ORDER_SIDE::BUY) );
+	orderQueue.putOrderInQueue( Order("Trader4","Stock4",250000,ORDER_SIDE::SELL) );
+	orderQueue.putOrderInQueue( Order("Trader7","Stock3",250000,ORDER_SIDE::BUY) );
+	orderQueue.putOrderInQueue( Order("Trader8","Stock4",250000,ORDER_SIDE::BUY) );
+
 
 
 	t.join();
